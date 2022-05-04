eef6f5894d7cc4e79b7f03b51b27b3bbd325c7c8
rajsahu2004 <101011482+rajsahu2004@users.noreply.github.com>
2022-05-04 10:10:01 +0530

    Add files via upload

---
+++ 30.03.2022 Lab Class/sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int  main(){
+	int m,n,i,sum=0;
+	prntf("Enter the initial and final number of the series : ");
+	scanf("%d%d",&m,&n);
+	for(i=m;i<=n;i++){
+		sum+=i;
+	}
+	printf("Sum is %d",sum);
+	return 0;
+}
